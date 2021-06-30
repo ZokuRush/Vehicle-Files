11e1d3079a337dbaaacfbe9a6baebca5b533ef70
Zoku <67478346+ZokuRush@users.noreply.github.com>
2021-06-30 15:06:05 -0600

    Add files via upload

---
+++ names.lua
@@ -0,0 +1,9 @@
+Citizen.CreateThread(function()
+    AddTextEntry("p1", "2014 McLaren P1")
+    AddTextEntry("super", "2014 McLaren P1")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("gtr", "2017 Nissian GTR")
+    AddTextEntry("sports", "2017 Nissian GTR")
+end)
