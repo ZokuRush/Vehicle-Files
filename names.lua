66fbb1ac4a70d8097b85388384bd8f16a93ac91a
Zoku <67478346+ZokuRush@users.noreply.github.com>
2021-06-30 15:05:53 -0600

    Delete names.lua

--- names.lua
+++
@@ -1,9 +0,0 @@
-Citizen.CreateThread(function()
-    AddTextEntry("p1", "2014 McLaren P1")
-    AddTextEntry("super", "2014 McLaren P1")
-end)
-
-Citizen.CreateThread(function()
-    AddTextEntry("gtr", "2017 Nissian GTR")
-    AddTextEntry("sports", "2017 Nissian GTR")
-end)
